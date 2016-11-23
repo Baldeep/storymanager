18f80f0277d0bbd7fc677d8bc739a4180faa8b9b
Baldeep <baldeepsingh.93@gmail.com>
2016-11-23 22:45:11 +0000

    begun work on navbar

--- storymanager/views.py
+++ storymanager/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 
 from .models import Mission
 # Create your views here.
+
+
 def mission_screen(request, mission_id):
 	try:
 		mission = Mission.objects.get(pk=mission_id)
 	except Mission.DoesNotExist:
 		mission = Mission()
-		return render(request, 'storymanager/missionscreen.html', {'mission' : mission})
-	return render(request, 'storymanager/missionscreen.html', {'mission' : mission})
+		return render(request, 'storymanager/mission_screen.html', {'mission' : mission})
+	return render(request, 'storymanager/mission_screen.html', {'mission' : mission})
 	
