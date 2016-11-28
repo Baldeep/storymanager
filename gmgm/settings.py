fba8dcc808f6b9a37e16c14de959181bed8c6938
Baldeep <baldeepsingh.93@gmail.com>
2016-11-28 22:12:26 +0000

    added flowchart making

--- gmgm/settings.py
+++ gmgm/settings.py
@@ -117,5 +117,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-
+import os.path
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.abspath(os.path.join(BASE_DIR, '..', 'media'))
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '..', 'static'))
